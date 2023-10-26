56be2a2a95f1a942d66c509aee6ac1ebc9198e5f
Adeline Ayinkamiye <adelinemug6@gmail.com>
2023-10-26 12:00:57 +0200

    refactor: add negative decimal binary converter

--- exercises/exercise5.js
+++ exercises/exercise5.js
@@ -1,12 +1,12 @@
 // function to convert a number to its binary representation
 
 const convertNumberToBinary = (num) => {
-  return num.toString(2);
+  return (num >>> 0).toString(2);
 };
 
 // function to calculate the Fibonacci sequence up to a given number
 
-const fibonacci = (num) =>{
-  if(num <= 1) return num
-  return fibonacci(num -1) + fibonacci(num -2)
-}
+const fibonacci = (num) => {
+  if (num <= 1) return num;
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
